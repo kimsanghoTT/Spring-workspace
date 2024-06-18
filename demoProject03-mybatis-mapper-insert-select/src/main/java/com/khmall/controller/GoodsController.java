c28b4639d853482d7e4aa6638f16e7ed57fbb821
kimsanghoTT <165999006+kimsanghoTT@users.noreply.github.com>
2024-06-18 16:09:07 +0900

    자잘한 수정

--- demoProject03-mybatis-mapper-insert-select/src/main/java/com/khmall/controller/GoodsController.java
+++ demoProject03-mybatis-mapper-insert-select/src/main/java/com/khmall/controller/GoodsController.java
@@ -24,8 +24,6 @@ public class GoodsController {
 	public String insertProduct(Goods goods, Model model){
 		
 		productService.insertProduct(goods);
-	//	List<Goods> productList = productService.getAllProduct();
-	//	model.addAttribute("productList",productList);
 		model.addAttribute("msg", "상품이 성공적으로 등록됐습니다.");
 		
 		//등록한 결과를 볼 수 있는 html로 이동
