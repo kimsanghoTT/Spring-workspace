ce703dd944a07b6bc908281f07e47eb35ea79e21
kimsanghoTT <165999006+kimsanghoTT@users.noreply.github.com>
2024-06-19 11:35:09 +0900

    select로 테이블 값 가져오기

---
+++ demoProject03-mybatis-mapper-select/src/main/java/com/khmall/DemoProject03MybatisMapperSelectApplication.java
@@ -0,0 +1,13 @@
+package com.khmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject03MybatisMapperSelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject03MybatisMapperSelectApplication.class, args);
+	}
+
+}
